daecebdcf27c7f89576bc63f543f2f3532f7fb91
Hoang Dang Khoa <khoahoang@cqtdt.asia>
2023-05-03 08:48:51 +0700

    k

--- commandtip.sh
+++ commandtip.sh
@@ -1,1 +1,12 @@
 echo ${PWD##*/} # get name parent folder
+
+# Thay đổi quyền với chmod
+chmod u=rwx,g=rwx,o=rwx dir1 <filename>
+
+# Thay đổi owner và group cần có quyền sudo
+sudo chown <username> <filename>
+sudo chown <groupname> <filename>
+sudo chown <username>:<groupname> <filename>
+
+# add user to group
+usermod -aG <group> <user>
